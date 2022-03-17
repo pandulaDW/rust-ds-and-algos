dee984c7c5b746dd5351b895f1672d222e0cc9a0
Pandula Weerasooriya <pandula.official@gmail.com>
2022-03-17 10:48:55 +0530

    Added iter implementation for binary tree

--- src/data_structures/binary_tree.rs
+++ src/data_structures/binary_tree.rs
@@ -27,6 +27,28 @@ impl<T: Ord> BinaryTree<T> {
             }
         }
     }
+
+    // creates an instance of TreeIter and walks the first left path before
+    // returning the iterator
+    pub fn iter(&self) -> TreeIter<T> {
+        let mut iter = TreeIter {
+            unvisited: Vec::new(),
+        };
+        iter.push_left_edge(self);
+        iter
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a BinaryTree<T>
+where
+    T: Ord,
+{
+    type Item = &'a T;
+    type IntoIter = TreeIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
 }
 
 /// A node of the binary tree
@@ -35,4 +57,33 @@ pub struct TreeNode<T> {
     element: T,
     left: BinaryTree<T>,
     right: BinaryTree<T>,
+}
+
+/// The state of an in-order traversal of a "BinaryTree"
+pub struct TreeIter<'a, T> {
+    unvisited: Vec<&'a TreeNode<T>>,
+}
+
+impl<'a, T> TreeIter<'a, T>
+where
+    T: Ord,
+{
+    fn push_left_edge(&mut self, mut tree: &'a BinaryTree<T>) {
+        while let BinaryTree::NonEmpty(ref node) = *tree {
+            self.unvisited.push(node);
+            tree = &node.left;
+        }
+    }
+}
+
+impl<'a, T> Iterator for TreeIter<'a, T>
+where
+    T: Ord,
+{
+    type Item = &'a T;
+    fn next(&mut self) -> Option<&'a T> {
+        let node = self.unvisited.pop()?;
+        self.push_left_edge(&node.right);
+        Some(&node.element)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,15 @@ mod data_structures;
 mod problems;
 mod recursion;
 
-fn main() {}
+use data_structures::binary_tree::BinaryTree;
+fn main() {
+    let mut tree = BinaryTree::Empty;
+    tree.add("jaeger");
+    tree.add("robot");
+    tree.add("droid");
+    tree.add("mecha");
+
+    for v in tree.into_iter() {
+        println!("{:?}", v);
+    }
+}
